9c54e48a9e2f4884886b52caa6a6f3893801f392
viniciu-pereira <viniciusfelipipereira@gmail.com>
2021-03-02 01:08:41 +0000

    1-3-21 busca binaria

--- 02-buscaSequencial-obj.mjs
+++ 02-buscaSequencial-obj.mjs
@@ -1,23 +1,32 @@
-/* 
-    BUSCA SEQUENCIAL EM OBJETOS
- */
-
- function buscaSequencial(vetor, valorBusca, fnComp){ //
+function buscaSequencial(vetor, fnComp){ //
     for(let i = 0; i < vetor.length; i++){
-        if(fnComp(vetor[i], valorBusca)) //em vez de fazer a comparacao dentro da funcao de busca, ela foitercerizada para uma outra funcao que foi chamada dentro dessa.
+        if(fnComp(vetor[i])) //em vez de fazer a comparacao dentro da funcao de busca, ela foitercerizada para uma outra funcao que foi chamada dentro dessa.
         return i
     }
     return -1 //valor de busca nao encontrado
 }
+
+
 
 /*
 function comparaNome(objNome, valorBusca) {
     return objNome.first_name === valorBusca
 }
 */
- //funcao acima em arrow
-const comparaNome = (objNome, valorBusca) => objNome.first_name === valorBusca
 
-import { objNomes} from './includes/vetor-obj-nomes.mjs'
+/* FUNCOES QUE FORMARAM A ARROW FUNCTION DENTRO DOS CONSOLE.LOG*/ 
+//const comparaNome = (objNome, valorBusca) => objNome.first_name === valorBusca
 
-console.log(buscaSequencial(objNomes,'Fausto',comparaNome))
+//const comparaGrupo = (objNome, valorBusca) => objNome.group_name === valorBusca
+
+import {objNomes} from './includes/vetor-obj-nomes.mjs'
+
+console.log(buscaSequencial(objNomes,'Maria', obj => obj.first_name === 'Fausto'))
+console.log(buscaSequencial(objNomes,'Maria', obj => obj.group_name === 'Maria'))
+
+//primeiro nome da lista que tem mais de 1000 registrados:
+let pos = buscaSequencial(objNomes, obj => obj.frequency_total > 1000)
+
+console.log(objNomes[pos])
+
+pos = buscaSequencial(objNomes, obj => obj.frequency_total > 10000 && obj.classification === 'M')

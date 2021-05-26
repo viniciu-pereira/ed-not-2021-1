006be609481dc3aa3ab0445129a35236815099a6
viniciu-pereira <viniciusfelipipereira@gmail.com>
2021-05-26 14:57:56 +0000

    (26-5-21) fim do exercicio 2

--- ex-estruturas02.mjs
+++ ex-estruturas02.mjs
@@ -3,3 +3,45 @@
 
     Data de entrega: 31/05, até 20h50, valendo nota de participação
 */
+
+class Palindromo {
+
+    #data 
+    #texto
+    #textoRev 
+
+    constructor() {
+        this.#data = []
+        this.#texto = []
+        this.#textoRev = []
+    }
+
+    insertBack(val) {
+        for(let i = 0; i < val.length; i++){
+            this.#data.push(val.charAt(i))
+        }
+        return this.#data
+    }
+
+    removeBack() {
+        while( this.#data ){
+            this.#textoRev += this.#data.pop()
+            this.#data--
+        }
+        return this.#textoRev
+    }
+
+    print() {
+        return JSON.stringify(this.#data)
+    }
+}
+
+let classe = new Palindromo()
+
+function executar(palin){
+
+    console.log(classe.insertBack(palin))
+    console.log(classe.removeBack())
+}
+
+executar('Socorram-me, subi no ônibus em Marrocos')

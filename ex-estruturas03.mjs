0c170e59851afca21ee19a63301d74fd89d43639
viniciu-pereira <viniciusfelipipereira@gmail.com>
2021-05-28 01:28:27 +0000

    (27-5-21) funcao de espalhamento

--- ex-estruturas03.mjs
+++ ex-estruturas03.mjs
@@ -13,3 +13,71 @@
 
     Data de entrega: 31/05, até 20h50, valendo nota de participação
 */
+class Node {
+    constructor(val) {
+        this.data = val     // Armazenamento de valor
+        this.next = null    // Ponteiro para o próximo nodo
+    }
+}
+
+class LinkedList {
+
+    #head       // Nodo inicial da lista
+    #tail       // Nodo final da lista
+    #count      // Quantidade de nodos na lista
+
+    constructor() {
+        this.#head = null       // Não tem nada no início
+        this.#tail = null       // Não tem nada no fim
+        this.#count = 0
+    }
+
+    get empty() {
+        return this.#count = 0
+    }
+
+    // Inserção em uma posição arbitrária
+    insert(val) {
+
+        // Cria no nodo para abrigar o valor e o endereço do próximo nodo
+        let inserted = new Node(val)
+
+        // inserção em lista vazia
+        if(this.empty) {
+            this.#head = inserted
+            this.#tail = inserted
+        }
+
+        
+        // posição intermediária
+        else if ( val > inserted ){
+            // Nodo ANTERIOR à posição de inserção
+            let before = this.#head
+            // Percorre a lista até encontar o nodo da posição ANTERIOR à de inserção
+            for(let i = 0; i < this.#count - 1; i++) before = before.next
+            // Nodo que irá ficar DEPOIS do inserido
+            let after = before.next
+            // O próximo do nodo inserido passa a ser o nodo after
+            inserted.next = after
+            // O próximo do nodo before passa a ser o nodo inserido
+            before.next = inserted
+
+            if( val < inserted){
+                this.#count++
+                this.#tail = inserted
+            }
+            else{
+                this.#count--
+                this.#head = inserted
+            }
+        
+        }
+        this.#count++
+        
+    }
+}
+let lista = new LinkedList()
+
+console.log(lista.insert(10))
+
+console.log(lista.insert(20))
---
+++ teste-organizacional.mjs
@@ -0,0 +1,7 @@
+let ns = [1,2,3,50,48,47,12,41,63,26,94,96,35,74,56]
+
+function ordenacao(a,b){
+    return a-b
+}
+
+console.log(ns.sort(ordenacao))
